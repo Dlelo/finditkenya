b29298c44ba1049d8efa37fba3075f3f15e19ec2
kievo23 <kelvinchege@gmail.com>
2018-04-04 16:31:02 +0300

    database migration

--- config/System.js
+++ config/System.js
@@ -1,3 +1,4 @@
 module.exports = {
-	db_uri: 'mongodb://viral:king2017@ds127126.mlab.com:27126/findit'
+	//db_uri: 'mongodb://viral:king2017@ds127126.mlab.com:27126/findit',
+	db_uri: 'mongodb://findit:RUgCvQ5fkpJQUsx9@localhost:27017/findit'
 }

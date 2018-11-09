899fb5d1ecb309f42f8a221451a1ff6096eba019
kev <kelvinchege@gmail.com>
2018-11-09 12:50:15 +0300

    youtube fix

--- routes/admin.js
+++ routes/admin.js
@@ -93,7 +93,11 @@ router.post('/edit/:id', role.auth, cpUpload, function(req, res, next) {
 	    b.facebook = req.body.facebook;
 	    b.twitter = req.body.twitter;
 	    b.instagram = req.body.instagram;
-	    b.youtube = req.body.youtube.replace("watch?v=", "embed/");
+      if(req.body.youtube){
+        if(req.body.youtube.includes("watch?v=")){
+          b.youtube = req.body.youtube.replace("watch?v=", "embed/");
+        }
+      }
 	    b.linkedin = req.body.linkedin;
 
 	    //Booking and Ordering

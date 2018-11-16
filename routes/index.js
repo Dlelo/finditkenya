507e7be878bb8903c8a7f4b2e9037edaf7cb5c80
dlelo <akinyidonattah@gmail.com>
2018-11-16 15:25:58 +0300

    categories

--- routes/index.js
+++ routes/index.js
@@ -336,10 +336,7 @@ router.get('/moment',function(req,res){
 
 // VIEW ALL CATEGORIES
 router.get('/viewcategories', function (req, res, next) {
-  var categories = Category.find({ approved: true, group: 'general' }).sort([['order', 1]]);
-  Promise.all([categories]).then(values => {
-    res.render('business/viewcategories', { title: "Find It: Categories", categories: values[0] });
-  });
+  res.render('business/viewcategories');
 });
 
 //ADVERTISING

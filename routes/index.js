d593d7b6696afd3560b28c7232281f2fcf3800dc
edwinwalela <edwinwalela@gmail.com>
2018-12-17 11:24:25 +0300

    search by keywords

--- routes/index.js
+++ routes/index.js
@@ -1061,7 +1061,6 @@ router.get('/newsearch',function(req,res){
       'spherical': true,
       "query":{
         $and:[
-            {branch:null},
           {$or:[ 
             {name:{
               $regex:query,
@@ -1087,11 +1086,6 @@ router.get('/newsearch',function(req,res){
       'spherical': true,
       "query":{
         $and:[{
-          $or:[
-            {branch:false},
-            {branch:null}
-          ]
-        },{
         $or:[
           {subcategory:{
           $regex:query,
@@ -1216,6 +1210,9 @@ router.get('/updatesearch',function(req,res){
   let multi = query.split(' ');
   let catSearch = [false,''];
   let subCatSearch = [false,''];
+  multi = multi.filter(function(word){
+    return word != 'in'
+  })
   for(let i = 0; i < multi.length; i++){
     if(CATS.includes(multi[i])){
       catSearch[0] = true;
@@ -1234,88 +1231,59 @@ router.get('/updatesearch',function(req,res){
     res.redirect('category/'+cat+'?lat='+lat+'&lon='+lon)
   } 
   
-  console.log()
   let point = {
     "type": "Point",
     "coordinates": [lon,lat]
   };
 
-  var q1 = Business.aggregate([{
-    '$geoNear': {
-      'near': point,
-      'spherical': true,
-      "query":{
-        $and:[
-          {$or:[{branch:null},{branch:false}]},
-          {$or:[ 
-            {name:{
-              $regex:query.substr(0,query.length/1.5),
-              $options:'i'
-              }},
-            {name:{
+var q1 = Business.aggregate([{
+  '$geoNear': {
+    'near': point,
+    'spherical': true,
+    "query":{
+      $and:[
+        {$or:[{branch:null},{branch:false}]},
+        {$or:[ 
+          {name:{
+            $regex:query.substr(0,query.length/1.5),
+            $options:'i'
+            }},
+            {keywords:{
               $regex:query,
               $options:'i'
             }},
-            {name:{
-              $regex:multi[0],
+            {keywords:{
+              $regex:multi[multi.length-1],
               $options:'i'
-              }},
-            
-            ]
-          }
-        ]
-      },
-      'distanceField': 'distance',
-      'maxDistance': 1000000000000
-    }
-  },{ "$sort": { "paid": -1 } }
+            }},
+            {name:{
+              $regex:multi[0].substr(0,multi[0].length/1.5),
+              $options:'i'
+            }},
+          ]
+        }
+      ]
+    },
+    'distanceField': 'distance',
+    'maxDistance': 1000000000000
+  }
+},{ "$sort": { "paid": -1 } }
 ]);
 
-  var q2 = Business.aggregate([{
-    '$geoNear': {
-      'near': point,
-      'spherical': true,
-      "query":{
-        $and:[{
-          $or:[
-            {branch:false},
-            {branch:null}
-          ]
-        },{
-        $or:[
-          {subcategory:{
-          $regex:query,
-          $options:'im'
-          }},
-          {description:{
-            $regex:query,
-            $options:'im'
-          }},
-        ]
-      }]
-    },
-      'distanceField': 'distance',
-      'maxDistance': 1000000000000
-    },
-    
-  },{ "$sort": { "paid": -1 } }
-]);
-  
   var categories = Category.find({group: 'general'}); 
 
-  Promise.all([q1,q2,categories]).then(values => {
+  Promise.all([q1,categories]).then(values => {
     
     var res1 = values[0].filter(function(biz){
-      return biz.name.trim().toLowerCase().startsWith(multi[0]) ||
-      biz.name.trim().toLowerCase().includes(query.substr(0,query.length/1.5))  
+      console.log(biz.name)
+      return biz.name.trim().toLowerCase().includes(multi[0].substr(0,multi[0].length/1.5)) || 
+      biz.name.trim().toLowerCase().startsWith(query.substr(0,3))
     })
-
-      res1 = res1.concat(values[1])
 
       res.render('business/search',{
         title: req.query.search,
         businesses: res1,
-        categories: values[2],
+        categories: values[1],
         host: req.get('host')
     });
   })

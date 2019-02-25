4161fa2a548c1281e46b521a8b5dc36f5a008441
root <kelvinchege@gmail.com>
2019-02-25 07:27:19 +0000

    changes on product page

--- routes/product.js
+++ routes/product.js
@@ -525,13 +525,11 @@ router.get('/:bizname/:category/:subcat/:minicat/:slug',function(req, res){
   Promise.all([categories,product]).then(values => {
   console.log(values[1].bizid.id);
   owner = values[1].bizid.user_id;
-  console.log(owner);
+
+
   console.log(owner.names);
   console.log(owner.phone);
   console.log(owner.email);
-
-
-
   //   User.findOne({ username: values[1].bizid.user_id }).populate('user_id').then(function(u){
   //     //if(err) console.log(err);
   //     console.log(values[1]);

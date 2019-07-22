9ef720b3d6bef0bc61ee57bc1f2f95ccd219b285
kev <kelvinchege@gmail.com>
2019-07-22 17:17:35 +0300

    products design page

--- routes/product.js
+++ routes/product.js
@@ -301,7 +301,7 @@ router.get('/shipping',function(req, res){
   });
   var categories = Category.find({group:'shopping'});
     Promise.all([categories]).then(values => {
-      res.render('procategory/duct/checkout',{
+      res.render('category/product/checkout',{
         cart: req.session.cart,
         total: total,
         categories: values[0]
